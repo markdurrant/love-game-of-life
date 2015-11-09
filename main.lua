b886f3262ac7a8dc6d169eb293523bbfc21e9976
Mark Durrant <mark.j.durrant.gmail.com>
2015-11-09 15:58:32 +0000

    start refactor

--- main.lua
+++ main.lua
@@ -1,100 +1,100 @@
 function love.load()
+  -- grid dimensions
+  -- ===============
   grid = {}
-  grid.cols = 16 * 8
-  grid.rows = 10 * 8
-  grid.size = 8
-  grid.gutter = 2
+  grid.cols = 5
+  grid.rows = 5
+  grid.size = 80 -- width & height of grid cells in pixels
+  grid.gutter = 10 -- size of gap between grid cells in pixels
 
+  -- color definitions
+  -- ==================
   color = {}
-  color.alive = {150, 150, 200}
-  color.dead = {50, 50, 50}
+  color.background = {0, 0, 0}
+  color.alive = {150, 150, 255}
+  color.dead = {30, 30, 50}
 
-  display = {}
-  display.x = grid.cols * grid.size
-  display.y = grid.rows * grid.size
-  display.mode = love.window.setMode(display.x, display.y)
+  -- display setup
+  -- =============
+  love.window.setMode(grid.cols * grid.size + grid.gutter, grid.rows * grid.size + grid.gutter)
 
-  organisms = {}
-  for i = 0, grid.rows - 1 do
-    local row = i
-    local y = i * grid.size
+  -- organisms setup
+  -- ===============
+  organismsGroup = {}
 
-    for i = 0 ,grid.cols - 1 do
-      local col = i
-      local x = i * grid.size
+  function randomBoolean()
+    if love.math.random() > 0.5 then
+      return true
+    else
+      return false
+    end
+  end
 
+  -- poulate organisms group
+  for r = 1, grid.rows do
+    for c = 1, grid.cols do
       organism = {}
-      organism.col = col
-      organism.row = row
-      organism.x = x
-      organism.y = y
+      organism.col = c
+      organism.row = r
       organism.isAlive = false
       organism.aliveNeighbors = 0
-      table.insert(organisms, organism)
+      table.insert(organismsGroup, organism)
     end
   end
 
-  function randomiseBoard()
-    for i, v in ipairs(organisms) do
-      if love.math.random() > 0.5 then
-        v.isAlive = true
-      end
+  -- create 'blinker' pattern
+  organismsGroup[12].isAlive, organismsGroup[13].isAlive, organismsGroup[14].isAlive = true, true, true
+
+  -- print all organisms for dev
+  function printAllOrganisms( ... )
+    for i,v in pairs(organismsGroup) do
+      -- split every row
+      if (i - 1) % 5 == 0 then print("---") end
+      -- print cell
+      print("C" .. v.col .. "_R" .. v.row .. "_alive:" .. tostring(v.isAlive))
+      -- split last cell
+      if i == table.getn(organismsGroup) then print("---") end
     end
   end
 
-  randomiseBoard()
+  -- cooldown counter for printAllOrganisms
+  printCooldown = 0
 
   function countNeighbors()
-    for i,v in ipairs(organisms) do
-      v.aliveNeighbors = 0
 
-      if v.row > 0 and organisms[i - grid.cols].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- north
-      if v.row > 0 and v.col < grid.cols - 1 and organisms[i - grid.cols + 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- north east
-      if v.col < grid.cols - 1 and organisms[i + 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- east
-      if v.row < grid.rows - 1 and v.col < grid.cols - 1 and organisms[i + grid.cols].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- south east
-      if v.row < grid.rows - 1 and organisms[i + grid.cols].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- south
-      if v.row < grid.rows - 1 and v.col > 0 and organisms[i + grid.cols - 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- south west
-      if v.col > 0 and organisms[i - 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- west
-      if v.row > 0 and v.col > 0 and organisms[i - grid.cols - 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- north west
-    end
   end
-
-  function switchState()
-    for i, v in ipairs(organisms) do
-      if v.isAlive == false and v.aliveNeighbors == 3 then
-        v.isAlive = true
-      elseif v.isAlive == true and v.aliveNeighbors < 2 then
-        v.isAlive = false
-      elseif v.isAlive == true and v.aliveNeighbors > 3 then
-        v.isAlive = false
-      end
-    end
-  end
-
-  timer = 0
 end
 
 function love.update(dt)
-  timer = timer + dt
+  printCooldown = printCooldown + dt
 
-  if timer >= 0.001 then
-    countNeighbors()
-    switchState()
-    timer = 0
-  end
-
-  if love.keyboard.isDown("r") then
-    randomiseBoard()
+  if love.keyboard.isDown("p") and printCooldown > 0.25 then
+    printAllOrganisms()
+    printCooldown = 0
   end
 end
 
 function love.draw()
-  for i, v in ipairs(organisms) do
-    if v.isAlive == true then love.graphics.setColor(color.alive) else love.graphics.setColor(color.dead) end
-    love.graphics.rectangle("fill", v.x, v.y, grid.size, grid.size)
+  -- draw organisms
+  -- ==============
+  for i, v in ipairs(organismsGroup) do
+    -- set organism x/y cords
+    local x = (v.col - 1) * grid.size + grid.gutter / 2
+    local y = (v.row - 1) * grid.size + grid.gutter / 2
 
+    -- set color based on organism alive/dead
+    if v.isAlive == true then
+      love.graphics.setColor(color.alive)
+    else
+      love.graphics.setColor(color.dead)
+    end
+
+    -- draw organism rectangle
+    love.graphics.rectangle("fill", x, y, grid.size, grid.size)
+
+    -- draw borders
+    love.graphics.setColor(color.background)
     love.graphics.setLineWidth(grid.gutter)
-    love.graphics.setColor(0, 0, 0)
-    love.graphics.rectangle("line", v.x, v.y, grid.size, grid.size)
+    love.graphics.rectangle("line", x, y, grid.size, grid.size)
   end
 end
---
+++ old-main.lua
@@ -0,0 +1,100 @@
+function love.load()
+  grid = {}
+  grid.cols = 16 * 8
+  grid.rows = 10 * 8
+  grid.size = 8
+  grid.gutter = 2
+
+  color = {}
+  color.alive = {150, 150, 200}
+  color.dead = {50, 50, 50}
+
+  display = {}
+  display.x = grid.cols * grid.size
+  display.y = grid.rows * grid.size
+  display.mode = love.window.setMode(display.x, display.y)
+
+  organisms = {}
+  for i = 0, grid.rows - 1 do
+    local row = i
+    local y = i * grid.size
+
+    for i = 0 ,grid.cols - 1 do
+      local col = i
+      local x = i * grid.size
+
+      organism = {}
+      organism.col = col
+      organism.row = row
+      organism.x = x
+      organism.y = y
+      organism.isAlive = false
+      organism.aliveNeighbors = 0
+      table.insert(organisms, organism)
+    end
+  end
+
+  function randomiseBoard()
+    for i, v in ipairs(organisms) do
+      if love.math.random() > 0.5 then
+        v.isAlive = true
+      end
+    end
+  end
+
+  randomiseBoard()
+
+  function countNeighbors()
+    for i,v in ipairs(organisms) do
+      v.aliveNeighbors = 0
+
+      if v.row > 0 and organisms[i - grid.cols].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- north
+      if v.row > 0 and v.col < grid.cols - 1 and organisms[i - grid.cols + 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- north east
+      if v.col < grid.cols - 1 and organisms[i + 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- east
+      if v.row < grid.rows - 1 and v.col < grid.cols - 1 and organisms[i + grid.cols].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- south east
+      if v.row < grid.rows - 1 and organisms[i + grid.cols].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- south
+      if v.row < grid.rows - 1 and v.col > 0 and organisms[i + grid.cols - 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- south west
+      if v.col > 0 and organisms[i - 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- west
+      if v.row > 0 and v.col > 0 and organisms[i - grid.cols - 1].isAlive then v.aliveNeighbors = v.aliveNeighbors + 1 end -- north west
+    end
+  end
+
+  function switchState()
+    for i, v in ipairs(organisms) do
+      if v.isAlive == false and v.aliveNeighbors == 3 then
+        v.isAlive = true
+      elseif v.isAlive == true and v.aliveNeighbors < 2 then
+        v.isAlive = false
+      elseif v.isAlive == true and v.aliveNeighbors > 3 then
+        v.isAlive = false
+      end
+    end
+  end
+
+  timer = 0
+end
+
+function love.update(dt)
+  timer = timer + dt
+
+  if timer >= 0.2 then
+    countNeighbors()
+    switchState()
+    timer = 0
+  end
+
+  if love.keyboard.isDown("r") then
+    randomiseBoard()
+  end
+end
+
+function love.draw()
+  for i, v in ipairs(organisms) do
+    if v.isAlive == true then love.graphics.setColor(color.alive) else love.graphics.setColor(color.dead) end
+    love.graphics.rectangle("fill", v.x, v.y, grid.size, grid.size)
+
+    love.graphics.setLineWidth(grid.gutter)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("line", v.x, v.y, grid.size, grid.size)
+  end
+end
